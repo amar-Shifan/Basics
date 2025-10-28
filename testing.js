52edd91ee077090bf519504da6c9f35ece72ef71
Amar shifan <amarshifaanvp@gmail.com>
2025-10-28 23:57:31 +0530

    added

--- testing.js
+++ testing.js
@@ -1,46 +1,111 @@
-function isPrime(num){
-    if(num < 2) return false
+// function isPrime(num){
+//     if(num < 2) return false
 
-    for(let i = 2 ; i < Math.sqrt(num) ; i++){
-        if(num%i === 0) return false
-    }
+//     for(let i = 2 ; i < Math.sqrt(num) ; i++){
+//         if(num%i === 0) return false
+//     }
 
-    return true
-}
+//     return true
+// }
 
-function fizzbuzz(){
-    let res = [];
-    for(let i = 1 ; i < 100 ; i++){
-        if(i%3 === 0 || i%5 === 0){
-            res.push(i)
-            if(isPrime(i)){
-                let next = i + 1
-                while(!isPrime(next)){
-                    next++
-                }
-                res.push(next)
-            }
-        }
-    }
+// function fizzbuzz(){
+//     let res = [];
+//     for(let i = 1 ; i < 100 ; i++){
+//         if(i%3 === 0 || i%5 === 0){
+//             res.push(i)
+//             if(isPrime(i)){
+//                 let next = i + 1
+//                 while(!isPrime(next)){
+//                     next++
+//                 }
+//                 res.push(next)
+//             }
+//         }
+//     }
+//     return res
+// }
+// console.log(fizzbuzz())
 
-    return res
-}
-console.log(fizzbuzz())
+// let str = 'amar is awesome'
+// let rev = ''
+// let word = ''
 
-let str = 'amar is awesome'
-let rev = ''
-let word = ''
+// for(let i = 0 ; i<str.length ; i++){
+//     if(str[i] !== ' '  && i !== str.length-1){
+//         word+=str[i]        
+//     } else {
+//         while(word){
+//             rev += word.slice(-1);
+//             word = word.slice(0 , -1)
+//         }
+//         rev += ' '
+//     }
+// }
 
-for(let i = 0 ; i<str.length ; i++){
-    if(str[i] !== ' '  && i !== str.length-1){
-        word+=str[i]        
-    } else {
-        while(word){
-            rev += word.slice(-1);
-            word = word.slice(0 , -1)
-        }
-        rev += ' '
-    }
-}
+// console.log(rev)
 
-console.log(rev)
+// // custom map
+// function myMap(arr , callback){
+//     let res = []
+//     for(let i = 0 ; i<arr.length ; i++){
+//         res.push(callback(arr[i]))
+//     }
+//     return res 
+// } 
+
+// const nums = [1,2,3,4,5];
+// console.log(myMap(nums , n => n*2))
+
+// Array.prototype.myMap = function(callback) {
+//     let arr = this
+//     let res = []
+//     for(let i = 0 ; i<arr.length ; i++){
+//         res.push(callback(arr[i]))
+//     }
+//     return res 
+// }
+
+// console.log(nums.myMap(n=> n*3))
+
+// function getName(name, callback){
+//     console.log(name)
+//     callback(12)
+// }
+// function gage(age,callback){
+//     console.log(age)
+//     callback('munnar')
+// }
+
+
+// getName('amar' , (age)=>{
+//     gage(age , (place) =>{
+//         console.log(place)
+//     })
+// })
+
+// function* genetor(){
+//     let i = 1
+//     while(true){
+        
+//         yield i
+        
+//         i+=2
+//     }
+// }
+// let gen = genetor()
+
+// function print(){
+//     console.log(gen.next().value);
+//     setTimeout(() => {
+//         print()
+//     }, 1000);
+// }
+// print()
+
+// let obj = {
+//     name : 'amar',
+//     age : 20,
+// }
+// let ref = structuredClone(obj);
+// ref.age = 10
+// console.log(obj.age)
